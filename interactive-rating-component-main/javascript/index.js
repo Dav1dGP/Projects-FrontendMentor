fbdda241c5c496ef13fe02424c9e6f5fbf7e26e7
DavidGaspar <david.gaspar.alfaz@gmail.com>
2024-05-28 09:58:41 +0200

    add function buttons

--- interactive-rating-component-main/javascript/index.js
+++ interactive-rating-component-main/javascript/index.js
@@ -2,9 +2,16 @@ const mainContainer = document.querySelector(".main-container");
 const thankContainer = document.querySelector(".thank-you");
 const buttonSubmit = document.getElementById("submit");
 const rating = document.getElementById("rating");
-const button = document.querySelectorAll(".btn");
+const buttons = document.querySelectorAll(".btn");
 
 buttonSubmit.addEventListener("click", () => {
     thankContainer.classList.remove("hidden");
     mainContainer.style.display = "none";
+});
+
+buttons.forEach((button) => {
+    button.addEventListener("click", () => {
+        rating.textContent = button.textContent;
+        // rating.innerHTML = button.innerHTML;
+    });
 });
